f34da4102776ca556f407843ca0ed86d6521f037
Srecko <Srecko@DESKTOP-NF4504R>
2023-11-22 14:37:58 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load and Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Enter Path here";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(388, 311);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(794, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 24);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "graph";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1903, 1157);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image im; Size imSize; Color[,] c;
+        int h, w; Color empty = Color.FromArgb(0, 0, 0, 0);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter = 0;   
+            Bitmap bitmap;
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "Choose Image(*.jpg, *.png)|*.jpg;*.png;";
+            string filePath = textBox1.Text;
+            if (opf.ShowDialog() == DialogResult.OK)
+                im = Image.FromFile(opf.FileName);
+
+            opf.Dispose();
+            h = im.Height;
+            w = im.Width;
+            Width = w + 100;
+            Height = h + 100;
+            imSize = new Size(w, h);
+            bitmap = new Bitmap(im, imSize);
+            BitmapToColorMatrix(bitmap);
+
+            pictureBox1.Show();
+            pictureBox1.Size = new Size(w,h);
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            
+            for (int i = 50; i < h; i += 60)
+            {
+                int max = -1;
+                int maxr = i;
+                for (int q = 0; q < 45; q++)
+                    if (i + q < h)
+                    {
+                        int count = 0;
+                        bool emp = true;
+                        for (int j = 0; j < w; j++)
+                            if (emp && c[i + q, j] != empty) { count++; emp = false; }
+                            else if (!emp && c[i+q, j] == empty) { emp = true; }
+                        
+                        if(max < count) { max = count; maxr = q; }
+                    }
+
+                i += maxr;
+                for (int j = 0; j < w; j += 2)
+                    if (c[i ,j] != empty)
+                        CreateAndExportBitmap(i, j, filePath);
+            }
+            for (int i = 0; i < h; i ++)
+                for (int j = 0; j < w; j++)
+                    if (c[i, j] != empty)
+                        CreateAndExportBitmap(i, j, filePath);
+        }
+        int counter = 0;
+        private void CreateAndExportBitmap(int i, int j, string path)
+        {
+            List<(Point, Color)> Points = new List<(Point, Color)>();
+            DFS(ref Points, i, j);
+            int t = - 1, b = 9999999, l= 99999999, r = -1;
+            foreach((Point, Color) p in Points)
+            {
+                int x = p.Item1.X;
+                int y = p.Item1.Y;
+                if (x > r) r = x;
+                if (x < l) l = x;
+                if (y > t) t = y;
+                if (y < b) b = y;
+            }
+            int width = r - l;
+            int height = t - b;
+            Bitmap tBm = new Bitmap(width + 1, height + 1);
+            for(int i2 = 0; i2 < height; i2++)
+                for(int j2 = 0; j2 < width; j2++)
+                    tBm.SetPixel(j2, i2, Color.FromArgb(0, 0, 0, 0));
+            
+            foreach ((Point, Color) p in Points)    
+            {
+                int width2 = p.Item1.X - l;
+                int height2 = p.Item1.Y - b;
+                tBm.SetPixel(width2, height2, p.Item2);
+            }
+            counter++;
+            string npath = path + "\\" + Convert.ToString(counter) + ".png";
+            textBox1.Text = path;
+            tBm.Save(npath);
+            tBm.Dispose();
+            if (checkBox1.Checked)
+            {
+                Bitmap cmap = new Bitmap(w, h);
+                for (int i2 = 0; i2 < h; i2++)
+                    for (int j2 = 0; j2 < w; j2++)
+                        cmap.SetPixel(j2, i2, c[i2, j2]);
+
+                pictureBox1.Image = cmap;
+                pictureBox1.Refresh();
+                cmap.Dispose();
+            }
+        }
+        private void DFS(ref List<(Point, Color)> Points, int i, int j)
+        {
+            Queue<Point> toGo = new Queue<Point>();
+            Point curP = new Point(j, i);
+            toGo.Enqueue(curP);
+            Points.Add((curP, c[i, j]));
+            while(toGo.Count > 0)
+            {
+                Point temp = toGo.Dequeue();
+                int x = temp.X, y = temp.Y;
+                if(x >= 0 && x < w && y >= 0 && y < h && c[y,x] != empty)
+                {
+                    Points.Add((temp, c[y, x]));
+                    c[y, x] = empty;
+                    int kek;
+                    if (y > h * 2 / 3) kek = 11;
+                    else kek = 2;
+
+                    for (int p = -kek; p <= kek; p++)
+                        for (int q = -kek; q <= kek; q++)
+                            if (p != 0 || q != 0)
+                                toGo.Enqueue(new Point(x + p, y + q));
+                }
+            }
+        }
+        private void BitmapToColorMatrix(Bitmap map)
+        {
+            c = new Color[h,w];
+            for (int i = 0; i < h; i++)
+                for (int j = 0; j < w; j++)
+                    c[i,j] = map.GetPixel(j, i);//x, y i = y, j = x
+        }
+    }
+}
